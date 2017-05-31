cb1d7738e3c4aa3286fa68ac81e665b7da9db9c8
theiconnz <theiconnz@gmail.com>
2017-05-31 21:05:33 +1200

    0.0.1

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Smallbusiness (https://theicon.co.nz/)
+ *
+ * Module
+ *
+ * PHP version 5
+ *
+ * @category Module
+ * @package  Theicon
+ * @author   Don Nuwinda <nuwinda@gmail.com>
+ * @license  GPL https://theicon.co.nz
+ * @link     https://theicon.co.nz
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Theiconnz_Smallbusiness',
+    __DIR__
+);
